fed2c04c05d1a3fafe6e34cc66a6c7c8b8ea53cd
Uros <urosivanovich@gmail.com>
2022-02-18 04:09:06 -0500

    last  test passed

--- api/task/model.js
+++ api/task/model.js
@@ -1,11 +1,7 @@
 const db = require('../../data/dbConfig')
 
 const getTasks = async () => {
-    
-//     select t.*, project_name, project_description
-//     from tasks as t
-//    left join projects as p
-//     on t.project_id = p.project_id;
+
     const rows = await db('tasks as t')
         .join('projects as p', 't.project_id', 'p.project_id')
         .select('t.*', 'project_name', 'project_description')
@@ -17,24 +13,18 @@ const getTasks = async () => {
        })
        return rows
 }
- 
+
 const postTask = async (task) => {
+
     const newTask = await db('tasks as t').insert(task)
     .then(([task_id]) => {
         return db('tasks')
         .where('task_id', task_id).first()
     })
-    if(newTask.task_completed == 0){
-        return{
-            ...task,
-            task_completed:false
-        }
-    }else{
-        return{
-            ...task,
-            task_completed:true
-        }
-    }
+    !newTask.task_completed ? 
+    newTask.task_completed=false :
+    newTask.task_completed=false
+    return newTask
 }
 
 module.exports = {
